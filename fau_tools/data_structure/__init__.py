810f130184442e0754bdfba1ec785a73b041a793
Fau <Fau818@qq.com>
2023-10-18 01:19:07 +0800

    Squashed commit of the following:
    commit 356e7c5d7d3c41c38a0dd2d53efa4769f606f508
    Author: Fau <Fau818@qq.com>
    Date:   Tue Oct 17 10:13:44 2023 +0800
    
        version: 2.0.0
        refactor!: remove `torch_tools.torch_train`
        feat: add `fau_tools.TaskRunner` to run a training

--- fau_tools/data_structure/__init__.py
+++ fau_tools/data_structure/__init__.py
@@ -1,1 +1,4 @@
-from ._data_structure import *
+from ._model_manager import ModelManager
+from ._scalar_recorder import ScalarRecorder
+from ._time_manager import TimeManager
+from ._task_runner import TaskRunner
